a1071cde3b8dea297567069a042bbea599e4f571
ben scarborough <benscarborough@scarflix.com>
2017-07-06 16:01:11 -0400

    The start of something

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+
+
+
+// const blurayDiscs = new Schema({
+//     name: {type: String, required: true, unique: false},
+//     genre: [String],
+//     year: Number,
+//     mpaaRating: [String],
+//     rottenTomatoScore: Number,
+//     specialFeatures: [{
+//         deletedScenes: Number,
+//         alternateEndings: null,
+//         audioCommentary: null
+//         }],
+//     runTime: Numbers
+// });
+
+
+
+module.exports = router;
